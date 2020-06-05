5487ab9931da4280583dc85eb56c9a00d945aaae
timiistimi <timilehin65@gmail.com>
2020-06-05 16:40:45 -0600

    initialized passport with jwt

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
   mongoURI: "mongodb://localhost/devconnector",
+  secretOrKey: "secret",
 };
--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
 const app = express();
+
+//body parser
+app.use(bodyParser.urlencoded({ extended: false }));
 
 //DB Config
 const db = require("./config/keys").mongoURI;
@@ -16,7 +22,11 @@ mongoose
   .then(() => console.log("MongoDB Connected...."))
   .catch((err) => console.log(err));
 
-app.get("/", (req, res) => res.send("Hello world"));
+//Passport Middleware
+app.use(passport.initialize());
+
+//Passport Config
+require("./config/passport")(passport);
 
 //User Routes
 app.use("/api/users", users);
