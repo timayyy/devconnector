e8064a1512f631c5c0976fcfbe424e0cdc5ff239
timiistimi <timilehin65@gmail.com>
2020-06-04 19:19:31 -0600

    initial commit with express and route files

---
+++ routes/api/posts.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+//@route    GET api/posts/test
+//@desc     test post route
+//@access   Public
+
+router.get("/test", (req, res) => {
+  res.json({ msg: "posts works" });
+});
+
+module.exports = router;
---
+++ routes/api/users.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const router = express.Router();
+
+//@route    GET api/users/test
+//@desc     test users route
+//@access   Public
+
+router.get("/test", (req, res) => {
+  res.json({ msg: "users works" });
+});
+
+module.exports = router;
